6b7e36aac777a45b0c0dd4c5956890ffbbadd643
Tom Barron <tusculum@gmail.com>
2019-11-26 12:55:54 -0500

    Test for functions with no __doc__ string

--- tests/test_pyppi.py
+++ tests/test_pyppi.py
@@ -1,10 +1,13 @@
 from pyppi import version
 import pyppi.__main__ as pmain
 import glob
+from importlib import import_module
+import inspect
 import pytest
 import tbx
 
 
+# -----------------------------------------------------------------------------
 def test_flake():
     """
     Check code quality
@@ -14,6 +17,36 @@ def test_flake():
     cmd = "flake8 --ignore $FLAKE_IGNORE {}".format(" ".join(files))
     result = tbx.run(tbx.expand(cmd))
     assert result == ""
+
+
+# -----------------------------------------------------------------------------
+def test_function_doc():
+    """
+    Verify that each of our functions have a __doc__ string
+    """
+    pytest.dbgfunc()
+
+    importables = ['pyppi.__main__']
+    importables.extend([tbx.basename(_).replace('.py', '')
+                        for _ in glob.glob('tests/*.py')])
+
+    missing_doc = []
+    for mname in importables:
+        mod = import_module(mname)
+        for name, obj in inspect.getmembers(mod, inspect.isclass):
+            if doc_missing(obj) and name not in missing_doc:
+                missing_doc.append(name)
+
+            for mthname, mthobj in inspect.getmembers(obj, inspect.isfunction):
+                if doc_missing(mthobj) and mthname not in missing_doc:
+                    missing_doc.append("{}.{}".format(name, mthname))
+
+        for name, obj in inspect.getmembers(mod, inspect.isfunction):
+            if doc_missing(obj) and name not in missing_doc:
+                missing_doc.append("{}.{}".format(mname, name))
+
+    if missing_doc:
+        pytest.fail("Items missing __doc__:\n   " + "\n   ".join(missing_doc))
 
 
 # -----------------------------------------------------------------------------
@@ -29,6 +62,7 @@ def test_version(capsys):
     assert version._v in result
 
 
+# -----------------------------------------------------------------------------
 def lglob(*args, dupl_allowed=False):
     """
     glob a list of paths and return the results in a single list
@@ -40,5 +74,15 @@ def lglob(*args, dupl_allowed=False):
     return rval
 
 
-def test_pytest_fails_successfully():
-    pytest.fail('getting started')
+# -----------------------------------------------------------------------------
+def doc_missing(obj):
+    """
+    Check *obj* for a non-empty __doc__ element
+    """
+    if not hasattr(obj, '__doc__') or obj.__doc__ is None:
+        return True
+    else:
+        return False
+
+
+# ==TAGGABLE==
