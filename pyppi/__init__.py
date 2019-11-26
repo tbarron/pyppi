289e9b6435441e386fdb2679c242e415433c62cc
Tom Barron <tusculum@gmail.com>
2019-11-26 14:59:24 -0500

    Add __init__.py so we can document the package in pydoc

---
+++ pyppi/__init__.py
@@ -0,0 +1,11 @@
+"""
+Build a python package index conforming to PEP 503
+
+pyppi build [-d] FILENAME
+
+    Build a python package index based on information in FILENAME.
+
+pyppi version [-d]
+
+    Report the version of the pyppi package.
+"""
