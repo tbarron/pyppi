2dfffd2733e3460c6369c1b5540e0c09499bc9db
Tom Barron <tusculum@gmail.com>
2019-11-26 18:06:08 -0500

    But importing local from py.path pulled in some doc-less functions
    So this update suppresses the complaints about local methods having no doc
    strings

--- tests/test_pyppi.py
+++ tests/test_pyppi.py
@@ -38,6 +38,8 @@ def test_function_doc():
                 missing_doc.append(name)
 
             for mthname, mthobj in inspect.getmembers(obj, inspect.isfunction):
+                if name == 'local':
+                    continue
                 if doc_missing(mthobj) and mthname not in missing_doc:
                     missing_doc.append("{}.{}".format(name, mthname))
 
